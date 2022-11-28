742a02604717f6ea3481369e811c3247b5c873f7
Manuela Alecio <manuelaalecio.dev@gmail.com>
2022-11-28 15:55:03 -0300

    FocusTimer

---
+++ js/index.js
@@ -0,0 +1,70 @@
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop,
+  buttonSoundOn,
+  buttonSoundOff,
+  minutesDisplay,
+  secondsDisplay
+} from "./elements.js"
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset,
+})
+
+const sound = Sound()
+
+buttonPlay.addEventListener('click', () => {
+  controls.play()
+  timer.countdown()
+  sound.pressButton()
+})
+
+buttonPause.addEventListener('click', () => {
+  controls.pause()
+  timer.hold()
+  sound.pressButton()
+})
+
+buttonStop.addEventListener('click', () => {
+  controls.reset()
+  timer.reset()
+  sound.pressButton()
+})
+
+buttonSoundOff.addEventListener('click', () => {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+  sound.bgMusicPause()
+})
+
+buttonSoundOn.addEventListener('click', () => {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+  sound.bgMusicStart()
+})
+
+buttonSet.addEventListener('click', () => {
+  let newMinutes = controls.getMinutes()
+
+  if (!newMinutes) {
+    timer.reset()
+    return
+  }
+
+  timer.updateDisplay(newMinutes, 0)
+  timer.updateMinutes(newMinutes)
+})
---
+++ js/sounds.js
@@ -0,0 +1,32 @@
+export default function() {
+  const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+  const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+  const bgMusic = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true")
+  buttonPressAudio.volume = 0.2
+  kitchenTimer.volume = 0.07
+  bgMusic.volume = 0.5
+  bgMusic.loop()
+
+  function pressButton() {
+    buttonPressAudio.play()
+  }
+
+  function timeEnd() {
+    kitchenTimer.play()
+  }
+
+  function bgMusicStart() {
+    bgMusic.play()
+  }
+
+  function bgMusicPause() {
+    bgMusic.pause()
+  }
+
+  return {
+    pressButton,
+    timeEnd,
+    bgMusicStart,
+    bgMusicPause
+  }
+}
